fbd7184ac2c68cae9d27cc11940bff3b5cb8e378
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-09-16 14:22:27 -0500

    Deleted comments

--- js/handleLogin.js
+++ js/handleLogin.js
@@ -13,30 +13,13 @@ loginForm.addEventListener("submit", (e) => {
     console.log(usernameElement.value)
     console.log(passwordElement.value)
 
-
-
-    //localStorage.setItem('uname', usernameElement.value)
     localStorage.setItem('pw', passwordElement.value)
 
     window.localStorage.setItem('uname', usernameElement.value);
 
-    // let authToken = localStorage.getItem('authToken')
-    // console.log("Auth token:: " + authToken)
-    // console.log("hello again")
-    // console.log(localStorage.getItem('authToken'))
 
     console.log("Inside handle login ::")
     console.log(window.localStorage.getItem("uname"))
 
     window.location.href = 'profile.html'
 })
-
-
-// function handleLogin() {
-
-
-//     console.log("Loggin in")
-//     console.log("Username :: " + usernameElement.value)
-//     console.log("Password :: " + passwordElement.value)
-
-// }
