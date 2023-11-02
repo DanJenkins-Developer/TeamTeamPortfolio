7d142f3cef762adede3f638527b39c3dce72ef5b
Daniel Jenkins <djenkinsjg0617@gmail.com>
2023-11-02 12:47:46 -0500

    Getting user data on profile

--- js/profile.js
+++ js/profile.js
@@ -9,18 +9,39 @@
 // //     window.location.href = 'login.html'
 // // }
 
-// document.addEventListener('DOMContentLoaded', function () {
+let welcomHeaderElement = document.getElementById('welcomeHeader')
 
-//     // if (!isAuthenticated()) {
-//     //     redirectToLogin();
-//     // }
+document.addEventListener('DOMContentLoaded', function () {
 
+    // if (!isAuthenticated()) {
+    //     redirectToLogin();
+    // }
+    const token = localStorage.getItem('token')
 
-//     // console.log("Inside handle profile ::")
-//     // console.log(window.localStorage.getItem("uname"))
+        // Set up the headers with the Bearer token
+    const headers = new Headers({
+        'Authorization': `Bearer ${token}`
+    });
 
-//     let welcomHeaderElement = document.getElementById('welcomeHeader')
-//     let username = localStorage.getItem('uname')
-//     welcomHeaderElement.innerHTML = `Welcome ${username}`
+    fetch('http://127.0.0.1:8000/users/me',{
 
-// })
+                method:'GET',
+                headers: headers // Pass headers
+            })
+            .then(response => response.json())
+        .then(data => {
+            console.log("Received data:", data);
+            welcomHeaderElement.innerHTML = `Welcome ${data.first_name + " " + data.last_name}`
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    // console.log("Inside handle profile ::")
+    // console.log(window.localStorage.getItem("uname"))
+
+    // let welcomHeaderElement = document.getElementById('welcomeHeader')
+    // let username = localStorage.getItem('uname')
+    // welcomHeaderElement.innerHTML = `Welcome ${username}`
+
+})
