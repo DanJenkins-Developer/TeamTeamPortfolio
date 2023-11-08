9343e3b5016668262562c360fbf468ee87ce2a3e
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-07 19:26:03 -0600

    Getting profile picture

--- js/profile.js
+++ js/profile.js
@@ -17,7 +17,7 @@
 // }
 
 let welcomHeaderElement = document.getElementById('welcomeHeader')
-
+let profilePic = document.getElementById("profile-pic")
 let current_user = JSON.parse(localStorage.getItem('current_user'))
 
 console.log(current_user)
@@ -26,6 +26,7 @@ console.log(current_user.last_name)
 console.log(current_user.phone_number)
 
 welcomHeaderElement.innerHTML = `Welcome ${current_user.first_name + " " + current_user.last_name}`
+profilePic.src = "http://dnfuqgbuy8ebx.cloudfront.net/" + current_user.profile_picture_link
 
 
 // if (!current_user) {
