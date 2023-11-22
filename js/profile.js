032276068faeab432d3a72f2a80015c978456443
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-22 16:51:46 -0600

    Enforcing login

--- js/profile.js
+++ js/profile.js
@@ -1,29 +1,15 @@
-// function isAuthenticated() {
-//     //localStorage.clear()
-//     console.log("IN IS AUTHENTICATED")
-//     console.log(localStorage.getItem('token'))
-//     return Boolean(localStorage.getItem('token'))
-// }
 
-// // function redirectToLogin() {
-// //     window.location.href = 'login.html'
-// // }
-
-// let hasData = () => {
-//     if (
-//         Boolean(localStorage.getItem('firstName')
-//         Boolean(loca)
-//     )
-// }
 
 let welcomHeaderElement = document.getElementById('welcomeHeader')
 let profilePic = document.getElementById("profile-pic")
 let current_user = JSON.parse(localStorage.getItem('current_user'))
+let token = localStorage.getItem('token')
 
 console.log(current_user)
 console.log(current_user.first_name)
 console.log(current_user.last_name)
 console.log(current_user.phone_number)
+
 
 welcomHeaderElement.innerHTML = `Welcome ${current_user.first_name + " " + current_user.last_name}`
 profilePic.src = "http://dnfuqgbuy8ebx.cloudfront.net/" + current_user.profile_picture_link
