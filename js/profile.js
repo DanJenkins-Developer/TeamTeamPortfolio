c36e358433e5028888c0c6d643b5a54bf2695649
Daniel jenkins <djenkinsjg0617@gmail.com>
2023-11-24 18:53:16 -0600

    style innit

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
-let welcomHeaderElement = document.getElementById('welcomeHeader')
-let profilePic = document.getElementById("profile-pic")
+let welcomHeaderElement = document.getElementById('profile-welcomeHeader')
+let profilePic = document.getElementById("profile-profile-pic")
 
 
 
